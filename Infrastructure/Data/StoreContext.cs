a3f88db5bda90369b8020db586acd8665516cc91
Niko Makridakis <nmak345@gmail.com>
2020-05-20 08:52:52 -0500

    added API architecture

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,7 +1,8 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infastructure.Data
+namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
@@ -11,5 +12,12 @@ namespace Infastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
