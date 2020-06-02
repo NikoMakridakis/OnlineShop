af4e0bdcb67538c46c992ac2cdf455a28be5aeff
Niko Makridakis <nmak345@gmail.com>
2020-06-02 07:53:01 -0500

    created cache attribute class

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,10 +8,8 @@ using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
