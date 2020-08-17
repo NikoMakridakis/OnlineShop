f96174f0b5b0807e820a8223ee6e38b00fdac4e6
Niko Makridakis <61632975+NikoMakridakis@users.noreply.github.com>
2020-08-17 06:37:13 -0500

    Update AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
